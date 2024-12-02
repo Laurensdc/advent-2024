f53158c5893220693158a66ddcfd44ccf746f5cc
Laurens <laurens.decock@inthepocket.com>
2024-12-02 15:52:40 +0100

    🧪 Add test for counting report safety

--- main_test.go
+++ main_test.go
@@ -25,3 +25,20 @@ func TestSimilarityScore(t *testing.T) {
 		t.Errorf("Expected\n%v\ngot\n%v\n", expected, output)
 	}
 }
+
+func TestReportSafety(t *testing.T) {
+	reports := [][]int{
+		{7, 6, 4, 2, 1},
+		{1, 2, 7, 8, 9},
+		{9, 7, 6, 2, 1},
+		{1, 3, 2, 4, 5},
+		{8, 6, 4, 4, 1},
+		{1, 3, 6, 7, 9},
+	}
+	expected := 2
+	output := countSafeReports(reports)
+
+	if output != expected {
+		t.Errorf("Expected\n%v\ngot\n%v\n", expected, output)
+	}
+}
