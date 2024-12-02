f5e7df4e5027febe01da4e8df33c3ef43b6eec51
Laurens <laurens.decock@inthepocket.com>
2024-12-02 15:34:43 +0100

    🧪 Add test for Similarity Score

--- main_test.go
+++ main_test.go
@@ -4,11 +4,22 @@ import (
 	"testing"
 )
 
-func TestMain(t *testing.T) {
+func TestCalculateTotalDistance(t *testing.T) {
 	list1 := []int{3, 4, 2, 1, 3, 3}
 	list2 := []int{4, 3, 5, 3, 9, 3}
 	output := calculateTotalDistance(list1, list2)
 	expected := 11
+
+	if output != expected {
+		t.Errorf("Expected\n%v\ngot\n%v\n", expected, output)
+	}
+}
+
+func TestSimilarityScore(t *testing.T) {
+	list1 := []int{3, 4, 2, 1, 3, 3}
+	list2 := []int{4, 3, 5, 3, 9, 3}
+	output := calculateSimilarityScore(list1, list2)
+	expected := 31
 
 	if output != expected {
 		t.Errorf("Expected\n%v\ngot\n%v\n", expected, output)
