b8814de46c5ca7e78b85575ce1617bea80746d4a
Laurens <laurens.decock@inthepocket.com>
2024-12-12 11:59:29 +0100

    ♻️ Extract horizontalStringCount function
    ✅ Add unit test for horizontalStringCount

--- day04.go
+++ day04.go
@@ -1,33 +1,37 @@
 package main
 
-import ()
+func invertString(str string) string {
+	// Convert string to rune slice, reverse it, and return as a new string
+	runes := []rune(str)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+// How many times does needle or eldeen (inverted)
+// occur in s.
+func horizontalStringCount(needle, s string) int {
+	count := 0
+	for i := range s[:len(s)-len(needle)] {
+		// Find needle
+		if s[i:i+len(needle)] == needle {
+			count++
+			// Find eldeen (inverted)
+		} else if s[i:i+len(needle)] == invertString(needle) {
+			count++
+		}
+	}
+	return count
+}
 
 func FindXmasWordCount(lines []string) int {
 	xmasCount := 0
 
 	for lineIndex, line := range lines {
+		// Horizontal scans for the word XMAS in line
+		xmasCount += horizontalStringCount("XMAS", line)
 		for charIndex := range line {
-			// Horizontal scans for the word XMAS in line
-			if charIndex < len(line)-3 {
-				// if line[charIndex:charIndex+4] == "XMAS" {
-				// 	xmasCount++
-				// } else if line[charIndex:charIndex+4] == "SAMX" {
-				// 	xmasCount++
-				// }
-
-				if line[charIndex] == 'X' &&
-					line[charIndex+1] == 'M' &&
-					line[charIndex+2] == 'A' &&
-					line[charIndex+3] == 'S' {
-					xmasCount++
-				} else if line[charIndex] == 'S' &&
-					line[charIndex+1] == 'A' &&
-					line[charIndex+2] == 'M' &&
-					line[charIndex+3] == 'X' {
-					xmasCount++
-				}
-
-			}
 
 			// Vertical scans
 			if lineIndex < len(lines)-3 {
--- day04_test.go
+++ day04_test.go
@@ -23,3 +23,13 @@ func TestFindXmasWordCount(t *testing.T) {
 	}
 
 }
+
+func TestHorizontalStringCount(t *testing.T) {
+	input := "xwj_`|fwoWORDwojjoDROWf'*wjofWORDWORDDROW__"
+	expected := 5
+	output := horizontalStringCount("WORD", input)
+
+	if output != expected {
+		t.Errorf("Expected\n%v\ngot\n%v\n", expected, output)
+	}
+}
