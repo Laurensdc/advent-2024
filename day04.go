6464f271ee5bf2f6e7e87d9a6c9eae0700cc490b
Laurens <laurens.decock@inthepocket.com>
2024-12-13 19:08:11 +0100

    ♻️ Extract verticalStringCount function
    ✅ And add unit test for it

--- day04.go
+++ day04.go
@@ -13,7 +13,13 @@ func invertString(str string) string {
 // occur in s.
 func horizontalStringCount(needle, s string) int {
 	count := 0
-	for i := range s[:len(s)-len(needle)] {
+	for i := range s {
+		// We will read len(needle) to the right,
+		// don't go out of bounds
+		if i > len(s)-len(needle) {
+			continue
+		}
+
 		// Find needle
 		if s[i:i+len(needle)] == needle {
 			count++
@@ -25,28 +31,49 @@ func horizontalStringCount(needle, s string) int {
 	return count
 }
 
+// How many times does needle or eldeen (inverted)
+// occur across lines (vertically downward)
+func verticalStringCount(needle string, lines []string) int {
+	count := 0
+
+	for i := range lines {
+		// We will read len(needle) down,
+		// don't go out of bounds
+		if i > len(lines)-len(needle) {
+			continue
+		}
+
+		// Loop over every char in the line
+		for charIndex := range lines[i] {
+
+			// Get the word by fetching characters "down"
+			// across lines
+			var wordBytes []byte = make([]byte, len(needle))
+			for n := range needle {
+				wordBytes[n] = lines[i+n][charIndex]
+			}
+
+			word := string(wordBytes)
+
+			if word == needle || word == invertString(needle) {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
 func FindXmasWordCount(lines []string) int {
 	xmasCount := 0
+
+	xmasCount += verticalStringCount("XMAS", lines)
 
 	for lineIndex, line := range lines {
 		// Horizontal scans for the word XMAS in line
 		xmasCount += horizontalStringCount("XMAS", line)
-		for charIndex := range line {
 
-			// Vertical scans
-			if lineIndex < len(lines)-3 {
-				if lines[lineIndex][charIndex] == 'X' &&
-					lines[lineIndex+1][charIndex] == 'M' &&
-					lines[lineIndex+2][charIndex] == 'A' &&
-					lines[lineIndex+3][charIndex] == 'S' {
-					xmasCount++
-				} else if lines[lineIndex][charIndex] == 'S' &&
-					lines[lineIndex+1][charIndex] == 'A' &&
-					lines[lineIndex+2][charIndex] == 'M' &&
-					lines[lineIndex+3][charIndex] == 'X' {
-					xmasCount++
-				}
-			}
+		for charIndex := range line {
 
 			// Diagonal scans
 			if lineIndex < len(lines)-3 && charIndex < len(line)-3 {
--- day04_test.go
+++ day04_test.go
@@ -21,13 +21,29 @@ func TestFindXmasWordCount(t *testing.T) {
 	if output != expected {
 		t.Errorf("Expected\n%v\ngot\n%v\n", expected, output)
 	}
-
 }
 
 func TestHorizontalStringCount(t *testing.T) {
 	input := "xwj_`|fwoWORDwojjoDROWf'*wjofWORDWORDDROW__"
 	expected := 5
 	output := horizontalStringCount("WORD", input)
+
+	if output != expected {
+		t.Errorf("Expected\n%v\ngot\n%v\n", expected, output)
+	}
+}
+
+func TestVerticalStringCount(t *testing.T) {
+	input := []string{
+		"wjfWjodDxx",
+		"wjfOjodRkx",
+		"wjWRjodOkD",
+		"ojODjodWkR",
+		"rjRWjodRkO",
+		"djDWjodRkW",
+	}
+	expected := 4
+	output := verticalStringCount("WORD", input)
 
 	if output != expected {
 		t.Errorf("Expected\n%v\ngot\n%v\n", expected, output)
